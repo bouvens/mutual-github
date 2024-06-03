bce1897b4dc0354f781fb3057c3687236837e3aa
Alex Demin <bouvens@gmail.com>
2024-06-03 23:36:42 +0500

    feat: Add followers links and counting

--- README.md
+++ README.md
@@ -1,36 +1,30 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Mutual GitHub Followers and Not
+
+It is a micro project to help you find mutual and not mutual followers on GitHub. This uses an official API with a token (not included).
 
 ## Getting Started
 
-First, run the development server:
+First, clone the project.
+
+Then add `.env` file with content similar to `.env.example` with
+
+```dotenv
+GITHUB_TOKEN=xxxyyyzzz
+```
+
+Where `xxxyyyzzz` should be your token that you can take from [settings](https://github.com/settings/tokens).
+
+And you are ready to run the development server:
 
 ```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Open [http://localhost:3001](http://localhost:3001) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+Start editing the page by modifying `app/page.jsx`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## Links
+* GitHub API for [followers](https://docs.github.com/en/rest/users/followers)
+* GitHub API for [pagination](https://docs.github.com/en/rest/using-the-rest-api/using-pagination-in-the-rest-api)
+* <a target="_blank" href="https://icons8.com/icon/92484/safety-collection-place">Connections</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
---
+++ src/app/page.jsx
@@ -0,0 +1,81 @@
+import styles from './page.module.css';
+import { Octokit } from 'octokit';
+
+const PER_PAGE_DEFAULT = 100;
+const auth = process.env.GITHUB_TOKEN;
+
+export const metadata = {
+  title: 'Mutual Github',
+  description: 'Check out and count your mutual and not mutual followers on GitHub.',
+}
+
+const octokit = new Octokit({
+  auth,
+});
+
+export default async function Home() {
+  // https://docs.github.com/en/rest/users/followers
+  const followers = await octokit.paginate('GET /user/followers', {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+    per_page: PER_PAGE_DEFAULT,
+  });
+  const followerLogins = new Set(followers.map(follower => follower.login));
+  // https://docs.github.com/en/rest/using-the-rest-api/using-pagination-in-the-rest-api
+  const following = await octokit.paginate('GET /user/following', {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+    per_page: PER_PAGE_DEFAULT,
+  });
+  const { mutual, notMutual } = following.reduce(({ mutual, notMutual }, currentFollowing) => {
+    if (followerLogins.has(currentFollowing.login)) {
+      mutual.push(currentFollowing);
+      followerLogins.delete(currentFollowing.login);
+    } else {
+      notMutual.push(currentFollowing);
+    }
+    return {
+      mutual,
+      notMutual,
+    };
+  }, {
+    mutual: [],
+    notMutual: [],
+  });
+  const testMutual = mutual.slice(0, 100);
+
+  return (
+    <main className={styles.main}>
+      <h2>
+        Not Mutual Following ({notMutual.length})
+      </h2>
+      <div className={styles.cards}>
+        {notMutual.map(follower => <p key={follower.login} className={styles.userCard}>
+          <a href={`https://github.com/${follower.login}`}><img
+            width={64}
+            src={follower.avatar_url}
+            alt={`${follower.login} avatar`}
+          /> {follower.login}
+          </a></p>)}
+      </div>
+
+      <h2>
+        Mutual Following ({testMutual.length})
+      </h2>
+      <p className={styles.enumerated}>{testMutual.map(follower => <a
+        href={`https://github.com/${follower.login}`}
+        key={follower.login}
+      >{follower.login}</a>)}</p>
+
+      <h2>
+        Not Followed Followers ({followerLogins.size})
+      </h2>
+      <p className={styles.enumerated}>{Array.from(followerLogins).map(login => <a
+        href={`https://github.com/${login}`}
+        key={login}
+      >{login}</a>)}</p>
+    </main>
+  );
+}
