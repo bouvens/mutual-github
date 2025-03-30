eab31ee59d170a77a3dc33f2a2de4281281b042e
Alex Demin <bouvens@gmail.com>
2025-03-30 22:33:56 +0500

    feat: add GitHub ribbon link

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,16 @@ const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <a href="https://github.com/bouvens/mutual-github">
+          <img
+            style={{ position: 'absolute', top: 0, right: 0, border: '0' }}
+            src="https://bouvens.github.io/images/github-forkme-ribbon-right.svg"
+            alt="Fork me on GitHub"
+          />
+        </a>
+      </body>
     </html>
   );
 }
